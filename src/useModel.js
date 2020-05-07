955370faf6dcfcf8331d23a18696ad889b3c8006
Matthew Phillips <phillipsm2@gmail.com>
2020-05-06 20:45:54 -0400

    make work in node

--- src/useModel.js
+++ src/useModel.js
@@ -3,7 +3,7 @@ import { Application } from './application.js';
 
 export function useModel(reducer, initialState) {
   let [state, send] = useReducer(reducer, initialState);
-  let {worker} = useContext(Application);
+  let {worker} = useContext(Application) || {};
 
   useEffect(() => {
     worker.addEventListener('message', ev => send(ev.data));
